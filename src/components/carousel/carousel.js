55ab4c5f9522f5e0b5229233e3df71667d83994d
shadrul <shadrul100@gmail.com>
2020-09-13 01:31:15 +0530

    carousel added

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -1,35 +1,54 @@
-import React, { Component } from "react";
-import Carousel from "react-bootstrap/Carousel";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import OwlCarousel from 'react-owl-carousel';
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel/dist/assets/owl.theme.default.css';
 import './carousel.css'
-
-export class Carousell extends Component {
-  render() {
+const options = {
+    items: 2,
+    margin : 15,
+    center : true,
+    loop :true,
+    autoplay:true,
+    autoplayHoverPause:true,
+    autoplaySpeed : 50,
+    responsive:{
+      0:{
+          items:1
+      },
+      600:{
+          items:2
+      },
+      1000:{
+          items:2
+      }
+  }
+};
+class Carousell extends React.Component{
+  render()
+  {
     return (
-      <Carousel className="carousel">
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://d8it4huxumps7.cloudfront.net/images/home-page-banner/5f47a80b2a54f_Web_Banner_FINACE_Reg_now_open.png?d=1280x500"
-            alt="First slide"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://d8it4huxumps7.cloudfront.net/images/home-page-banner/5f47a80b2a54f_Web_Banner_FINACE_Reg_now_open.png?d=1280x500"
-            alt="Third slide"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://d8it4huxumps7.cloudfront.net/images/home-page-banner/5f47a80b2a54f_Web_Banner_FINACE_Reg_now_open.png?d=1280x500"
-            alt="Third slide"
-          />
-        </Carousel.Item>
-      </Carousel>
-    );
+      <div class="content">
+    <OwlCarousel
+    className="owl-theme"
+
+    {...options}
+>
+
+  <div class="item">
+  <img alt="img1" src="/assets/img1.png" class="image"/></div>
+  <div class="item">
+  <img alt="img1" src="/assets/img2.jpg" class="image"/></div>
+  <div class="item">
+  <img alt="img1" src="/assets/img3.png" class="image"/></div>
+  <div class="item">
+  <img alt="img1" src="/assets/img4.jpg" class="image"/></div>
+
+</OwlCarousel>
+</div>
+    )
   }
 }
+ReactDOM.render(<Carousell />, document.getElementById('root'));
 
 export default Carousell;
